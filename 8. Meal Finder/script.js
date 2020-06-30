6d4662bfa0e560dd8192e3ab988a47dacfbbae9d
octofoo <michael.loffelmann@protonmail.com>
2020-06-30 12:52:33 +0200

    display meal details

--- 8. Meal Finder/script.js
+++ 8. Meal Finder/script.js
@@ -39,7 +39,7 @@ function searchMeal(eventObject) {
                     mealsElement.innerHTML = data.meals.map(meal => `
                         <div class="meal">
                             <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                            <div class="meal-info" data-mealid="${meal.idMeal}">
                                 <h3>${meal.strMeal}</h3>
                             </div>
                         </div>
@@ -55,9 +55,83 @@ function searchMeal(eventObject) {
     }
 }
 
+// Get meal by ID
+function getMealByID(mealID) {
+    // fetch
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        // convert to JSON
+        .then(response => response.json())
+        // get data
+        .then(data => {
+            // get meal from response array
+            const meal = data.meals[0];
+            // call
+            addMealToDOM(meal);
+        });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+    // initialize array
+    const ingredientsArray = [];
+    // loop
+    for (let i = 1; i <= 20; i++) {
+        // check for ingredient
+        if (meal[`strIngredient${i}`]) {
+            // push to array
+            ingredientsArray.push(
+                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+            );
+        } else {
+            break;
+        }
+    }
+    // output
+    singleMealElement.innerHTML = `
+        <div class="single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">\
+            <div class="single-meal-info">
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+            </div>
+            <div class="main">
+                <p>${meal.strInstructions}</p>
+                <h2>Ingredients:</h2>
+                <ul>
+                    ${ingredientsArray.map(ingredient => `<li>${ingredient}</li>`).join("")}
+                </ul>
+            </div>
+        </div>
+    `;
+}
+
 
 
 // EVENT LISTENERS
 
 // Search button
 searchButton.addEventListener("click", searchMeal);
+
+// Meals
+mealsElement.addEventListener("click", eventObject => {
+    // loop through child elements with find
+    const mealInfo = eventObject.path.find(element => {
+        // check for class
+        if (element.classList) {
+            // return elements with "meal-info" class
+            return element.classList.contains("meal-info");
+        } else {
+            return false;
+        }
+    });
+    // check for meal info
+    if (mealInfo) {
+        // get meal ID
+        const mealID = mealInfo.getAttribute("data-mealid");
+        // call
+        getMealByID(mealID);
+    } else {
+        return false;
+    }
+});
