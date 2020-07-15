e5be1a8099b6312dfd92b2f0a6235da3e4ededce
octofoo <michael.loffelmann@protonmail.com>
2020-07-15 11:08:37 +0200

    add countsown and game over

--- 12. Typing Game/script.js
+++ 12. Typing Game/script.js
@@ -3,7 +3,7 @@ const word = document.getElementById("word");
 const text = document.getElementById("text");
 const scoreElement = document.getElementById("score");
 const timeElement = document.getElementById("time");
-const endGameElement = document.getElementById("end-game");
+const endGameElement = document.getElementById("end-game-container");
 const settingsButton = document.getElementById("settings-button");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
@@ -18,6 +18,9 @@ let score = 0;
 
 // Initial time
 let time = 10;
+
+// Time interval
+const timeInterval = setInterval(updateTime, 1000);
 
 
 
@@ -43,9 +46,37 @@ async function displayWord() {
 
 // Update score
 function updateScore() {
+    // increment score
     score++;
+    // display new score
     scoreElement.innerText = score;
 }
+
+// Update time
+function updateTime() {
+    // decrease time
+    time--;
+    // display new time
+    timeElement.innerText = `${time} s`;
+    // check for time running out
+    if (time === 0) {
+        // clear timer
+        clearInterval(timeInterval);
+        // display end game screen
+        gameOver();
+    }
+}
+
+// Game over
+function gameOver() {
+    endGameElement.innerHTML = `
+    <h1>Game over</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Play again</button>
+    `;
+    endGameElement.style.display = "flex";
+}
+
 
 
 
@@ -61,10 +92,18 @@ text.addEventListener("input", () => {
         displayWord();
         // update score
         updateScore();
+        // update time
+        time += 5;
+        updateTime();
     }
 });
 
 
 
 // CALLS
+
+// Set focus on input
+text.focus();
+
+// Display word
 displayWord();
